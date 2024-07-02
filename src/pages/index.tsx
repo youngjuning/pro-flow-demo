a48672d034dcf794816bc5484acd23bc941bf3fd
紫升 <yangjunning.yjn@alibaba-inc.com>
2024-07-02 19:38:30 +0800

    布局

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,18 @@
-import { FlowEditor, FlowEditorProvider, useFlowEditor } from '@ant-design/pro-flow';
+import { FlowEditor, FlowEditorProvider, Position, useFlowEditor } from '@ant-design/pro-flow';
 import { useEffect } from 'react';
 import { StringRender } from './StringNode';
 import styles from "./index.module.less";
-
 
 const ProFlowDemo = () => {
   const editor = useFlowEditor();
 
   useEffect(() => {
     editor.addNode({
-      id: 'a1',
-      type: 'StringNode',
-      position: { x: 0, y: 100 },
-      data: {
-        title: 'String Node',
-        handles: {},
-      },
+      id: 'horizontal-1',
+      sourcePosition: Position.Right,
+      type: 'input',
+      data: { label: 'Input' },
+      position: { x: 0, y: 80 },
     });
   }, [editor]);
 
